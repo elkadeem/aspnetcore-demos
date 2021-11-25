654a5091b677cbe86e232eb47f3f6ac056ecaf96
Wael Elkadim <elkadeem@hotmail.com>
2021-11-25 11:08:22 +0200

    Add student create action and its view

---
+++ MVC/FirtMVC.App/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using FirtMVC.App.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirtMVC.App.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student student)
+        {
+            if (!ModelState.IsValid)
+                return View(student);
+
+            ViewBag.Message = "Succsseded";
+            return View(student);
+        }
+    }
+}
---
+++ MVC/FirtMVC.App/Entities/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirtMVC.App.Entities
+{
+    public class Student
+    {
+        [Display(Name = "الأسم")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "اسم الأب")]
+        public string FatherName { get; set; }
+
+        [Display(Name = "اسم الجد")]
+        public string MiddleName { get; set; }
+
+        [Display(Name = "العائلة")]
+        public string FamilyName { get; set; }
+
+        [Display(Name = "تاريخ الميلاد")]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+        [Display(Name = "المحافظة")]
+        public string Gov { get; set; }
+
+        [Display(Name = "البريد الإلكتروني")]
+        public string Email { get; set; }
+
+        [Display(Name = "النوع")]        
+        public Gender Gender { get; set; }
+
+        [Display(Name = "اللغات التي يدرسها")]
+        public List<string> Languages { get; set; }
+
+        [Display(Name = "ملاحظات")]
+        [DataType(DataType.MultilineText)]
+        public string Notes { get; set; }
+    }
+
+    public enum Gender
+    {
+        [Display(Name = "ذكر")]
+        Male,
+        [Display(Name = "أنثي")]
+        Female
+    }
+}
