0c5302108ead68b18944fee9d8d80e0748caa0b4
Wael Elkadim <elkadeem@hotmail.com>
2021-11-29 16:14:33 +0200

    Add Authentication

---
+++ AuthWithoutIIdentity/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using AuthWithoutIIdentity.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthWithoutIIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> Login()
+        {
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "user1@mail.com"),
+                    new Claim("FullName", "FullName"),
+                    new Claim(ClaimTypes.Role, "Administrator"),
+                };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = true,
+                IssuedUtc = DateTime.UtcNow,
+                ExpiresUtc = DateTime.UtcNow.AddDays(1)
+            };
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AuthWithoutIIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthWithoutIIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
